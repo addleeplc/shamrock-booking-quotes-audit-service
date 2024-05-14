9d027311fa43b22f75f22d22840ef59d33429f91
shalyganov <g.shalyganov@haulmont.com>
2024-05-14 16:11:01 +0200

    fixed Supplier dto

--- modules/core/src/main/java/com/haulmont/shamrock/booking/quotes/audit/model/booking/Supplier.java
+++ modules/core/src/main/java/com/haulmont/shamrock/booking/quotes/audit/model/booking/Supplier.java
@@ -20,19 +20,8 @@ public class Supplier {
     @JsonProperty("code")
     private String code;
 
-    @JsonProperty("name")
-    private String name;
-
-    @JsonProperty("services")
-    private Service service;
-
     @JsonProperty("driver_ref")
     private Driver driverReference;
-
-    public Supplier(UUID id, String code) {
-        this.id = id;
-        this.code = code;
-    }
 
     public UUID getId() {
         return id;
@@ -48,22 +37,6 @@ public class Supplier {
 
     public void setCode(String code) {
         this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Service getService() {
-        return service;
-    }
-
-    public void setService(Service service) {
-        this.service = service;
     }
 
     public Driver getDriverReference() {
